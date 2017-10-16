8dd5c258ba7fdc40fbc6adb8a71c2355b7de1549
Andrey Zimovnov <zimovnov@gmail.com>
2017-10-16 03:55:11 +0300

    let it be

--- week6/utils.py
+++ week6/utils.py
@@ -11,7 +11,20 @@ import pickle
 
 
 def image_center_crop(img):
-    raise NotImplementedError()
+    h, w = img.shape[0], img.shape[1]
+    pad_left = 0
+    pad_right = 0
+    pad_top = 0
+    pad_bottom = 0
+    if h > w:
+        diff = h - w
+        pad_top = diff - diff // 2
+        pad_bottom = diff // 2
+    else:
+        diff = w - h
+        pad_left = diff - diff // 2
+        pad_right = diff // 2
+    return img[pad_top:h-pad_bottom, pad_left:w-pad_right, :]
 
 
 def decode_image_from_buf(buf):
