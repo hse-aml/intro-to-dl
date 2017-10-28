2c16b77bbb4ad6292ccb3801d5b50a65fa68440d
Andrey Zimovnov <zimovnov@gmail.com>
2017-10-29 02:03:11 +0300

    - move resource linkers to download_utils - cross-platform resource linking

--- download_utils.py
+++ download_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import shutil
 import tqdm
 import requests
 
@@ -25,3 +26,32 @@ def sequential_downloader(version, fns, target_dir):
     os.makedirs(target_dir, exist_ok=True)
     for fn in fns:
         download_from_github(version, fn, target_dir)
+
+
+def link_all_files_from_dir(src_dir, dst_dir):
+    os.makedirs(dst_dir, exist_ok=True)
+    for fn in os.listdir(src_dir):
+        src_file = os.path.join(src_dir, fn)
+        dst_file = os.path.join(dst_dir, fn)
+        if os.name == "nt":
+            shutil.copyfile(src_file, dst_file)
+        else:
+            if not os.path.exists(dst_file):
+                os.symlink(os.path.abspath(src_file), dst_file)
+
+
+def link_all_keras_resources():
+    link_all_files_from_dir("../readonly/keras/datasets/", os.path.expanduser("~/.keras/datasets"))
+    link_all_files_from_dir("../readonly/keras/models/", os.path.expanduser("~/.keras/models"))
+
+
+def link_week_3_resources():
+    link_all_files_from_dir("../readonly/week3/", ".")
+
+
+def link_week_4_resources():
+    link_all_files_from_dir("../readonly/week4/", ".")
+
+
+def link_week_6_resources():
+    link_all_files_from_dir("../readonly/week6/", ".")
