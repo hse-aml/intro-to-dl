28fc11c2dadf884399139ca9c9c1557b6990bc65
Andrey Zimovnov <zimovnov@gmail.com>
2017-10-29 00:07:00 +0300

    - remove parallel downloader, tqdm fails on Windows in thread - remove shell mkdir

--- download_utils.py
+++ download_utils.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import threading
 import os
 import tqdm
 import requests
-
-kill_thread = threading.Event()
 
 
 def download_from_github(version, fn, target_dir):
@@ -15,40 +12,16 @@ def download_from_github(version, fn, target_dir):
     full_fn = os.path.join(target_dir, fn)
     with open(full_fn, 'wb') as f:
         bar = tqdm.tqdm_notebook(total=total_size, unit='B', unit_scale=True)
+        bar.set_description(fn)
         for chunk in r.iter_content(32*1024):
             f.write(chunk)
             bar.update(len(chunk))
-            if kill_thread.is_set():
-                # master asks to stop
-                break
     if os.path.getsize(full_fn) != total_size:
         # not full download
         os.remove(full_fn)
 
 
-def parallel_downloader(version, fns, target_dir):
-    kill_thread.clear()
-    os.system("mkdir -p {0}".format(target_dir))
-    threads = []
-    try:
-        for fn in fns:
-            t = threading.Thread(target=download_from_github, args=(version, fn, target_dir))
-            t.daemon = True
-            t.start()
-            threads.append(t)
-        for t in threads:
-            t.join()
-    except KeyboardInterrupt:
-        print("Killed")
-    finally:
-        kill_thread.set()
-        for t in threads:
-            t.join()
-    print("Done")
-
-
 def sequential_downloader(version, fns, target_dir):
-    kill_thread.clear()
-    os.system("mkdir -p {0}".format(target_dir))
+    os.makedirs(target_dir, exist_ok=True)
     for fn in fns:
         download_from_github(version, fn, target_dir)
