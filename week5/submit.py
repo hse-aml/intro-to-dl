a45380b2c3c259276b645e20277d0b084962aba1
Andrey Zimovnov <zimovnov@gmail.com>
2017-10-29 02:18:49 +0300

    - fix missing np

--- week5/submit.py
+++ week5/submit.py
@@ -1,5 +1,5 @@
 import sys
-import os 
+import numpy as np
 sys.path.append("..")
 import grading
 
@@ -10,7 +10,6 @@ def submit_char_rnn(submission, score, email, token):
     grader = grading.Grader("cULEpp2NEeemQBKZKgu93A")
     history, samples = submission
     assert len(samples) == 25
-    grader.set_answer("pttMO", int(np.mean(history[:10]) > np.mean(history[-10:])) )
+    grader.set_answer("pttMO", int(np.mean(history[:10]) > np.mean(history[-10:])))
     grader.set_answer("uly0D", len(set(samples)))
     grader.submit(email, token)
-
