2a7dc3a650e903c7c1c544b2977e26fdde786a75
Andrey Zimovnov <zimovnov@gmail.com>
2017-11-06 22:34:27 +0300

    fix incomplete download handling

--- download_utils.py
+++ download_utils.py
@@ -9,15 +9,19 @@ import requests
 def download_file(url, file_path):
     r = requests.get(url, stream=True)
     total_size = int(r.headers.get('content-length'))
-    with open(file_path, 'wb') as f:
-        bar = tqdm.tqdm_notebook(total=total_size, unit='B', unit_scale=True)
-        bar.set_description(os.path.split(file_path)[-1])
-        for chunk in r.iter_content(32 * 1024):
-            f.write(chunk)
-            bar.update(len(chunk))
-    if os.path.getsize(file_path) != total_size:
-        # not full download
-        os.remove(file_path)
+    try:
+        with open(file_path, 'wb', buffering=16*1024*1024) as f:
+            bar = tqdm.tqdm_notebook(total=total_size, unit='B', unit_scale=True)
+            bar.set_description(os.path.split(file_path)[-1])
+            for chunk in r.iter_content(32 * 1024):
+                f.write(chunk)
+                bar.update(len(chunk))
+    except Exception:
+        print("Download failed")
+    finally:
+        if os.path.getsize(file_path) != total_size:
+            os.remove(file_path)
+            print("Removed incomplete download")
 
 
 def download_from_github(version, fn, target_dir):
