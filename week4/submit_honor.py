c6fb531d627f2417578e4fe6330acaa7c27a2610
Andrey Zimovnov <zimovnov@gmail.com>
2017-11-30 21:57:36 +0300

    honor submit script

---
+++ week4/submit_honor.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append("..")
+import grading
+
+
+def submit_honor(submission, email, token):
+    grader = grading.Grader("FCl7G51lEeeeZQ4xJ2nzLA")
+    D, G = submission
+    grader.set_answer("ryO01", D.output_shape[1])
+    grader.set_answer("PmSxZ", G.output_shape[1:])
+    grader.submit(email, token)
