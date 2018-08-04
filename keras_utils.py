f8c31376b2758830a4a38862162367ea5132e1d1
Andrey Zimovnov <zimovnov@gmail.com>
2018-08-04 20:54:46 +0300

    reset session with growing gpu ram

--- keras_utils.py
+++ keras_utils.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import keras
 import tqdm
 from collections import defaultdict
 import numpy as np
 from keras.models import save_model
+import tensorflow as tf
+import keras
+from keras import backend as K
 from tqdm_utils import SimpleTqdm
 
 
@@ -64,3 +66,16 @@ class ModelSaveCallback(keras.callbacks.Callback):
         model_filename = self.file_name.format(epoch)
         save_model(self.model, model_filename)
         print("Model saved in {}".format(model_filename))
+
+
+# !!! remember to clear session/graph if you rebuild your graph to avoid out-of-memory errors !!!
+def reset_tf_session():
+    curr_session = tf.get_default_session()
+    # close current session
+    if curr_session is not None:
+        curr_session.close()
+    K.clear_session()  # clear graph
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    s = K.set_session(tf.InteractiveSession(config=config))
+    return s
