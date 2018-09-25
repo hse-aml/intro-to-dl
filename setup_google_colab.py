601c96b2dfd763feec40ca6e7c24c80df1143b6e
Andrey Zimovnov <zimovnov@gmail.com>
2018-09-26 00:42:49 +0300

    fail-safe port exposure

--- setup_google_colab.py
+++ setup_google_colab.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import json
 import time
+import requests
 
 
 def download_github_code(path):
@@ -83,10 +84,35 @@ def setup_keras():
     download_utils.download_all_keras_resources("../readonly/keras/models", "../readonly/keras/datasets")
 
 
+def _get_ngrok_tunnel():
+    while True:
+        try:
+            tunnels_json = requests.get("http://localhost:4040/api/tunnels").content
+            public_url = json.loads(tunnels_json)['tunnels'][0]['public_url']
+            return public_url
+        except Exception:
+            print("Can't get public url, retrying...")
+            time.sleep(2)
+
+
+def _warmup_ngrok_tunnel(public_url):
+    while requests.get(public_url).status_code >= 500:
+        print("Tunnel is not ready, retrying...")
+        time.sleep(2)
+
+
 def expose_port_on_colab(port):
+    os.system("apt-get install net-tools")
+    # check that port is open
+    while not (":{} ".format(port) in str(subprocess.check_output("netstat -vatn", shell=True))):
+        print("Port {} is closed, retrying...".format(port))
+        time.sleep(2)
+
+    # run ngrok
     os.system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
     os.system("unzip ngrok-stable-linux-amd64.zip")
     os.system("./ngrok http {0} &".format(port))
-    tunnels_json = subprocess.check_output("curl -s http://localhost:4040/api/tunnels", shell=True)
-    time.sleep(5)
-    print("Open {0} to access your {1} port".format(json.loads(tunnels_json)['tunnels'][0]['public_url'], port))
+    public_url = _get_ngrok_tunnel()
+    _warmup_ngrok_tunnel(public_url)
+
+    print("Open {0} to access your {1} port".format(public_url, port))
